342f1b65528366216fe07fb03cce7fddca174844
Juan Pablo Babnik <juan_babnik@uca.edu.ar>
2022-01-10 17:42:10 -0300

    Modificar layout app

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,7 +11,6 @@
                 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/estilos.css')}}" rel="stylesheet" >    
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
@@ -56,7 +55,7 @@
         </div>
         <div class="footer text-center text-white font-weight-bold bg-dark p-2 mt-2">
             <p>Todos los derechos reservados &copy {{ date('Y') }}</p>            
-        </div>
+        </div>        
         <script src="{{ asset('js/funciones.js') }}"></script>
     </body>
 </html>
